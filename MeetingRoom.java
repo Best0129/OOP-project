ed746b5684ea897b42641d55726c8849582d2e55
Best0129 <117960323+Best0129@users.noreply.github.com>
2023-04-13 17:35:22 +0700

    Add files via upload

---
+++ MeetingRoom.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class MeetingRoom {
+    private JDesktopPane dp;
+    private JInternalFrame frame1, frame2, frame3;
+    private JButton log, reg, ok, back, participant, organizer, diagram_img, language;
+    private JTextField txt_user, txt_password, txt_confirm_pass, txt_meeting_name, txt_num_seat, txt_price_seat, txt_link_image;
+    private JLabel user, password, confirm, or, participant_txt, meeting_name, seat, place, rem_seat,price_seat, organizer_txt, image_link, num_seat;
+    private JComboBox cb_seat, cb_place;
+    public MeetingRoom(){
+
+    }
+}
